6f493146a96e2cd492163b1853e7610d03dfbbcb
Kevin Lin <developer@kevinlin.info>
2017-12-31 15:48:16 -0800

    Fix JSX syntax highlighting by using Prism highlighter instead

--- src/app/react/components/markdown/code-block.js
+++ src/app/react/components/markdown/code-block.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { colors, Spacing, Text } from 'react-elemental';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { atomOneDark } from 'react-syntax-highlighter/styles/hljs';
+import SyntaxHighlighter from 'react-syntax-highlighter/prism';
+import { atomDark } from 'react-syntax-highlighter/styles/prism';
 
 /**
  * Rendering component for a block of code (enclosed by three backticks).
@@ -19,9 +19,11 @@ const CodeBlock = ({ language, value }) => (
     >
       <SyntaxHighlighter
         language={language}
-        style={atomOneDark}
+        style={atomDark}
         customStyle={{
+          borderRadius: 0,
           fontFamily: 'inherit',
+          margin: 'inherit',
           padding: '24px 32px',
         }}
         codeTagProps={{
