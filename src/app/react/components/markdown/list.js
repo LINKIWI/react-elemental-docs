7f09482e076d24013fa6e58a330a3f09ea3843bf
Kevin Lin <developer@kevinlin.info>
2018-01-01 14:40:37 -0800

    Slight padding under each list item

--- src/app/react/components/markdown/list.js
+++ src/app/react/components/markdown/list.js
@@ -5,21 +5,29 @@ import { Spacing, Text } from 'react-elemental';
 /**
  * Rendering component for a list of elements.
  */
-const List = ({ ordered, children }) => (
-  <Spacing size="small" bottom>
-    <Text inline>
-      {ordered ? (
-        <ol style={{ padding: '0px 40px' }}>
-          {children}
-        </ol>
-      ) : (
-        <ul style={{ padding: '0px 40px' }}>
-          {children}
-        </ul>
-      )}
-    </Text>
-  </Spacing>
-);
+const List = ({ ordered, children }) => {
+  const items = children.map((li, idx) => (
+    <Spacing size="tiny" key={li.key} bottom={idx < children.length - 1}>
+      {li}
+    </Spacing>
+  ));
+
+  return (
+    <Spacing size="small" bottom>
+      <Text inline>
+        {ordered ? (
+          <ol style={{ padding: '0px 40px' }}>
+            {items}
+          </ol>
+        ) : (
+          <ul style={{ padding: '0px 40px' }}>
+            {items}
+          </ul>
+        )}
+      </Text>
+    </Spacing>
+  );
+};
 
 List.propTypes = {
   // True for <ol> lists; false for <ul> lists
