18488467aaabe5c1ee88e713deb6f1b44c6fe0fe
Kevin Lin <developer@kevinlin.info>
2017-12-31 16:09:59 -0800

    Root container updates Redux store with window dimensions

--- src/app/react/root.js
+++ src/app/react/root.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import routes from 'app/react/routes';
+import { setWindowDimensions } from 'app/redux/actions/context';
 
 /**
  * Application root, containing some abstractions that would otherwise be hardcoded into HTML.
  */
-const Root = () => (
-  <div>
-    <Helmet>
-      <meta charSet="UTF-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-      />
-    </Helmet>
+class Root extends Component {
+  static propTypes = {
+    handleWindowDimensionsChange: PropTypes.func.isRequired,
+  };
 
-    {routes}
-  </div>
-);
+  componentWillMount() {
+    window.addEventListener('resize', this.onResize);
 
-export default Root;
+    this.onResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = () => this.props.handleWindowDimensionsChange(window.innerWidth, window.innerHeight);
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+          />
+        </Helmet>
+
+        {routes}
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  handleWindowDimensionsChange: (width, height) => dispatch(setWindowDimensions(width, height)),
+});
+
+export default connect(null, mapDispatchToProps)(Root);
