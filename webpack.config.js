3b621c4fbaa676f35c1168b02a7073a1413c56f0
Kevin Lin <developer@kevinlin.info>
2018-03-19 12:34:52 -0500

    Upgrade to webpack@4

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const dotenv = require('dotenv');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
@@ -11,7 +12,6 @@ dotenv.config();
 // Array of known environment variables whose value may be injected into the frontend as a key in
 // process.env, allowing Node-like environment variable access in client-side logic.
 const BUILD_ENV_VARS = [
-  'NODE_ENV',
   'GIT_SHA',
   'REACT_ELEMENTAL_VERSION',
   'ROUTING_BASE_PATH',
@@ -25,6 +25,7 @@ const BUILD_ENV_VARS = [
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  mode: isProduction ? 'production' : 'development',
   entry: {
     main: './src/resources/templates/entry.js',
   },
@@ -50,6 +51,20 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+  performance: {
+    hints: false,
+  },
   plugins: [
     new webpack.ProgressPlugin(),
     new webpack.DefinePlugin({
@@ -63,11 +78,9 @@ module.exports = {
       inlineSource: '.js$',
     }),
     new HtmlWebpackInlineSourcePlugin(),
-    isProduction && new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
-    isProduction && new webpack.optimize.UglifyJsPlugin({
-      comments: false,
-    }),
   ].filter(Boolean),
+  devServer: {
+    historyApiFallback: true,
+    publicPath: '/',
+  },
 };
